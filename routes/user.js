be5a597b39bde35e65b0e5756c8094b23f6c6d22
zack <aminuzack7@gmail.com>
2019-05-24 02:53:09 +0100

    [finishes #166110370 write more test and refactored the signup endpoint]

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,61 @@ const moment = require('moment');
 const jwt = require('jsonwebtoken');
 
 
+
+//signup endpoint
+router.post('/', async (req, res) => {
+  const {error} = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const user = await User.find(e => e.email === req.body.email);
+  if (user) return res.status(400).send(`This email ${req.body.email} is already taken`);
+  
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const userToCreate = {
+       id : User.length + 1,
+       first_name : req.body.first_name,
+       last_name : req.body.last_name,
+       email : req.body.email,
+       password : hashedPassword,
+       created_at : moment().format('MMM Do YYYY, h:mm:ss a'),
+   
+    };
+
+    try {
+      const result = await User.push(userToCreate);
+      if ( result ) {
+        const token = jwt.sign(
+          {
+            id: userToCreate.id,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+          },
+          "jwtPrivateKey"
+        );
+          res.status(200).send({
+            status : 200,
+            data : {
+                token : token,
+                id : userToCreate.id,
+                first_name: userToCreate.first_name,
+                last_name : userToCreate.last_name,
+                email: userToCreate.email,
+                created_at : userToCreate.created_at
+               
+            }
+          });
+      }
+    } catch (err) {
+      res.send({
+        status : 500,
+        error: err.message          
+      });
+    }
+    
+});
+
+
+
 //get all users
 router.get('/getUsers', async (req, res) => {
   const users = User.map(({id, first_name, last_name, email, is_admin, created_at}) => 
@@ -19,56 +74,6 @@ router.get('/:id',  async ({ params: { id } }, res) => {
   res.send(user);
 });
 
-router.post('/', async (req, res) => {
-    const {error} = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    const user = await User.find(e => e.email === req.body.email);
-    if (user) return res.status(400).send(`This email ${req.body.email} is already taken`);
-    
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const userToCreate = {
-         id : User.length + 1,
-         first_name : req.body.first_name,
-         last_name : req.body.last_name,
-         email : req.body.email,
-         password : hashedPassword,
-         created_at : moment().format('MMM Do YYYY, h:mm:ss a'),
-     
-      };
-
-      try {
-        const result = await User.push(userToCreate);
-        if ( result ) {
-          const token = jwt.sign(
-            {
-              id: userToCreate.id,
-              first_name: req.body.first_name,
-              last_name: req.body.last_name
-            },
-            "jwtPrivateKey"
-          );
-            res.status(200).send({
-              status : 200,
-              data : {
-                  token : token,
-                  id : userToCreate.id,
-                  first_name: userToCreate.first_name,
-                  last_name : userToCreate.last_name,
-                  email: userToCreate.email,
-                  created_at : userToCreate.created_at
-                 
-              }
-            });
-        }
-      } catch (err) {
-        res.send({
-          status : 500,
-          error: err.message          
-        });
-      }
-      
-});
 
  
 module.exports = router;
--- spec/tests/unit/auth-signup.spec.js
+++ spec/tests/unit/auth-signup.spec.js
@@ -1,7 +1,9 @@
 const request = require('supertest');
 const {validate} = require('../../../models/user')
 let server; 
-
+const user = {
+    email: "zackaminu@yahoo.com"
+}
 
 describe('/api/v1/auth/signup', () => {
     beforeEach(() => {server = require('../../../index'); })
@@ -13,5 +15,32 @@ describe('/api/v1/auth/signup', () => {
            expect(response.status).toBe(400);
           })
 
+          it('should return 400 if one of the required user model is not provided', async () => {
+            const response = await request(server).post('/api/v1/auth/signup')
+            .send({ first_name: "zackaminu" })
+            .end((err, res) => {
+                expect(res.status).toBe(400);
+            })
+           })
+
+           it('should return 400 if email already exist', async () => {
+            const response = await request(server).post('/api/v1/auth/signup')
+            .send({ email: "zackaminu@yahoo.com" })
+            .end((err, res) => {
+                expect(res.body).toContain(user);
+            })
+           })
+
+           it("should return 200 ok if sign up is successful", async () => {
+            const res = await request(server).post('/api/v1/auth/signup')
+               .send({
+                    first_name: 'Zack', 
+                    last_name: 'Aminu',
+                    email: "sadiatu@gmail.com",
+                    password : "1166117"
+                })
+                expect(res.status).toBe(200);
+
+        })
  })
 })
