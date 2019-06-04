e2517ec0f08758330fb6ae92699b2cb96de6fcc1
zack <aminuzack7@gmail.com>
2019-06-04 19:36:14 +0100

    [finishes #166401852] image upload with multer and cloudinary available

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ require('express-async-errors');
 const express = require("express");
 const config = require("config");
 const app = express();
+const upload = require('./middleware/multer');
+const cloudinary = require('cloudinary');
+require("./config/cloudinaryConfig");
+
+
+app.post("/upload", upload.single('image'), async (req, res) => {
+  console.log("file", req.file);
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  res.send(result);
+  
+});
 
 
 require("./startup/cors")(app);
