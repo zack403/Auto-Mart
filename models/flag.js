18250554a9daa28ebfb01ebd1ab3615866ce1317
zack <aminuzack7@gmail.com>
2019-06-23 17:32:30 +0100

    [finish #166860841 integrate database and create flag ad endpoint and write unit test for the endpoint]

--- models/flag.js
+++ models/flag.js
@@ -2,8 +2,6 @@ const Joi = require('joi');
 const db = require('../startup/db');
 
 
-const flagSchema = [];
-
 const flagTable = async () => {
   return await db.query(`
   CREATE TABLE IF NOT EXISTS
@@ -15,22 +13,41 @@ const flagTable = async () => {
      owner SERIAL NOT NULL,
      created_on TIMESTAMP NOT NULL DEFAULT NOW(),
      PRIMARY KEY (id),
+     FOREIGN KEY (car_id) REFERENCES cars (id) ON DELETE CASCADE,
      FOREIGN KEY (owner) REFERENCES users (id) ON DELETE CASCADE
    )`);
  } 
-
-
-
 const validateFlagObj = flag => {
     const schema = {
         car_id : Joi.number().integer().required(),       
         reason : Joi.string().required(),
         description : Joi.string().required()
       };
-    
       return Joi.validate(flag, schema);
 }
-
-module.exports.Flags = flagSchema;
+const flagMethods =  {
+  findById: async (id) => {
+    const text = 'SELECT * FROM flags where id = $1'
+    return await db.query(text, [id]);
+  },
+  delete: async () => {
+    const text = 'DELETE FROM flags'
+    return await db.query(text);
+  },
+  save: async (owner, car_id, reason, description) => {
+      const text = `INSERT INTO
+      flags(owner, car_id, reason, description)
+      VALUES($1, $2, $3, $4)
+      returning *`;
+      const values = [
+        owner,
+        car_id,
+        reason,
+        description
+      ]
+     return await db.query(text, values);
+  }
+}
+module.exports.Flags = flagMethods;
 module.exports.validate = validateFlagObj;
 module.exports.flagTable = flagTable;
