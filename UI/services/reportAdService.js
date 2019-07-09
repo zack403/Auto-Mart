c45b981df39bc5467cfd640b57f90102697384b9
zack <aminuzack7@gmail.com>
2019-07-09 10:28:32 +0100

    [start #167158937] updated report ad logic

--- UI/services/reportAdService.js
+++ UI/services/reportAdService.js
@@ -1,11 +1,22 @@
 import {apiUrl} from "../config.js";
 import {fetchService} from "./fetchService.js";
+import {authService} from "./authService.js";
 
 
 export class ReportAdService {
     static async reportAD(formData) {
         let url = apiUrl("flag");
         const response = await fetch(url, fetchService("POST", formData));
+        return response.json();
+    }
+
+    static async getReportAD(id) {
+        let url = apiUrl(`flag/${id}`);
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': authService.getUserToken(),
+            }
+        });
         return response.json();
     }
 }
