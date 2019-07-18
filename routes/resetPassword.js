6b6e3b14db5767a87c704c1d6f9b8e5e6872345e
zack <aminuzack7@gmail.com>
2019-07-18 23:25:21 +0100

    updated reset passowrd route

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -4,8 +4,11 @@ const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 const config = require('config');
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 const errorResponse =  require('../helper/errorResponse');
 const { User } = require('../models/user');
+const resourceResponse = require('../helper/getAllResourceResponse');
+
 
 
 const transporter = nodemailer.createTransport(sgTransport({
@@ -15,23 +18,29 @@ const transporter = nodemailer.createTransport(sgTransport({
 }));
 
 let clientError;
+let message;
 
 
 router.post('/:user_email/reset_password', async (req, res) => {
-
-  const userEmail = req.params.user_email;
+  const email = req.params.user_email;
+  const {error: emailError} = validateEmail(email);
+  if(emailError) return res.status(400).send(clientError = errorResponse(400, error.details[0].message));
+  
+  const {error: passwordError} = validatePassword(req.body);
+  if(passwordError) return res.status(400).send(clientError = errorResponse(400, error.details[0].message));
+  
   const { rows: user } = await User.findByEmail(userEmail);
-  if (!user[0]) return res.status(404).send('You do not have an account with us');
+  if (!user[0]) return res.status(404).send(clientError = errorResponse(400, 'The email you provided does not exist with us'));
 
   // the user is a valid user set the user new password here..
   const { password, confirm_password } = req.body;
   if (password != null && confirm_password != null) {
     const {error} = validatePassword(req.body);
-    if(error) return res.status(400).send(clientError = errorResponse(400, error.details[0].message))
+    if(error) return res.status(400).send(clientError = errorResponse(400, error.details[0].message));
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(confirm_password, salt);
     const { rows: resetPassword } = await User.updateUser(user[0].id, hashedPassword);
-    if (resetPassword[0]) return res.status(200).send('Password successfully changed');
+    if (resetPassword[0]) return res.status(200).send(message = resourceResponse(200, 'Password successfully changed'));
   }
 
   // the user is a valid user, send an email containing the users password
@@ -62,6 +71,12 @@ const validatePassword = req => {
     password: Joi.string().min(7).alphanum().max(255).required(),
     confirm_password: Joi.string().valid(Joi.ref('password')).required().strict().min(7)
        .alphanum().max(255),
+  }
+  return Joi.validate(req, schema);
+}
+const validateEmail = req => {
+  const schema = {
+    email: Joi.string().required().email()
   }
   return Joi.validate(req, schema);
 }
