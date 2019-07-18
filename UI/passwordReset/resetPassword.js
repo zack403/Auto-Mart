cc4026ce707f417c9280b5eac8b141eb964eaeac
zack <aminuzack7@gmail.com>
2019-07-18 22:40:47 +0100

    [finishes #167385016] implemented frontend logic to reset password

---
+++ UI/passwordReset/resetPassword.js
@@ -0,0 +1,53 @@
+import {authService} from "../services/authService.js";
+
+
+let errorAlert = document.getElementById("alert-danger");
+let spinner = document.querySelector(".spinner");
+let alertSuc =  document.getElementById("alert-success");
+
+
+errorAlert.style.display = "none";
+alertSuc.style.display = "none";
+spinner.style.display = "none";
+
+const domDo = () => {
+    errorAlert.style.display = "none";
+    alertSuc.style.display = "none";
+    errorAlert.innerHTML = "";
+    alertSuc.innerHTML = "";
+}
+
+const resetPassword = async () => {
+    domDo();
+    spinner.style.display = "block";
+    let password = document.getElementById("newPassword").value;
+    let confirm_password = document.getElementById("confirmPassword").value;
+    let email = document.getElementById("email").value;
+
+    const formData = {
+        password,
+        confirm_password
+    }
+    try {
+        const result = await authService.resetPassword(email, formData);
+        console.log(result);
+        const {error, data} = result;
+        if(data) {
+            spinner.style.display = "none";
+            alertSuc.style.display = "block";
+            alertSuc.innerHTML = data;
+            return window.location.href = "../signin/sign-in.html";
+        }
+        else if(error){
+            spinner.style.display = "none";
+            errorAlert.style.display = "block";
+            errorAlert.innerHTML = error;
+        }
+    } catch (error) {
+        spinner.style.display = "none";
+        errorAlert.style.display = "block";
+        errorAlert.innerHTML = error.message;
+    }
+}
+
+document.getElementById("resetP").addEventListener("click", resetPassword)
